319dee636ebc1db044ff34d6c81087089b27a7df
Mahidhar Tanniru <87277264+Mahidhartanniru@users.noreply.github.com>
2025-05-13 00:43:00 -0700

    Delete includes directory

--- includes/includes/configuration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-raw_folder_path = 'abfss://raw@formula1dlshu.dfs.core.windows.net/'
-processed_folder_path='abfss://processed@formula1dlshu.dfs.core.windows.net/'
-presentation_folder_path='abfss://presentation@formula1dlshu.dfs.core.windows.net/'
